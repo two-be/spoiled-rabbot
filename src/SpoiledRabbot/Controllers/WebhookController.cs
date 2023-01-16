72aa40f6eefd61607294e612d67361bb23e4e7fe
Two <Two.Logician@icloud.com>
2023-01-16 18:12:09 +0700

    :loud_sound:

--- src/SpoiledRabbot/Controllers/WebhookController.cs
+++ src/SpoiledRabbot/Controllers/WebhookController.cs
@@ -24,10 +24,17 @@ public class WebhookController : ControllerBase
     }
 
     [HttpPost("Dialogflow")]
-    public async Task<ActionResult<DialogflowWebhookResponse>> PostForDialogflow([FromBody] DialogflowWebhookRequest value)
+    public async Task<IActionResult> PostForDialogflow([FromBody] DialogflowWebhookRequest value)
     {
         _logger.LogInformation("__{value}__", value.ToJson());
         await _line.ReplyAsync(value.OriginalDetectIntentRequest.Payload.Data.ReplyToken, "Hello, World!");
+        return Ok();
+    }
+
+    [HttpPost("Dialogflow/Explore")]
+    public IActionResult PostForDialogflowForExplore([FromBody] object value)
+    {
+        _logger.LogInformation("__{value}__", value);
         return Ok();
     }
 
@@ -35,6 +42,17 @@ public class WebhookController : ControllerBase
     public async Task<IActionResult> PostForLine([FromBody] LineWebhookRequest value)
     {
         _logger.LogInformation("__{value}__", value.ToJson());
+        using var client = _http.CreateClient();
+        var rs = await client.PostAsJsonAsync($"https://dialogflow.cloud.google.com/v1/integrations/line/webhook/{_settings.Dialogflow.AgentId}", value);
+        var content = await rs.Content.ReadAsStringAsync();
+        _logger.LogInformation("__{content}__", content);
+        return Ok();
+    }
+
+    [HttpPost("Line/Explore")]
+    public async Task<IActionResult> PostForLineForExplore([FromBody] object value)
+    {
+        _logger.LogInformation("__{value}__", value);
         using var client = _http.CreateClient();
         var rs = await client.PostAsJsonAsync($"https://dialogflow.cloud.google.com/v1/integrations/line/webhook/{_settings.Dialogflow.AgentId}", value);
         var content = await rs.Content.ReadAsStringAsync();
